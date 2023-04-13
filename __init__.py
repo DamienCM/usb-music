13e35f5120b97675ca3c1841ad3b9f51cf015a26
Damien CM <damien.cartiermillon@gmail.com>
2023-04-13 19:05:46 +0200

    yotube dl test3

--- __init__.py
+++ __init__.py
@@ -252,10 +252,10 @@ class USBMusicSkill(CommonPlaySkill):
         os.mkdir('/home/pi/.usb-music/temp')
         bash_command = f'yt-dlp -x -f mp4 "ytsearch1:{phrase}" --geo-bypass --metadata-from-title "(?P<artist>.+?) - (?P<title>.+)" --xattrs --embed-thumbnail -o /home/pi/.usb-music/temp/request'
         LOG.info(f"YTB-DL :  {bash_command}")
-        process = subprocess.run(bash_command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        LOG.info(f"YTB-DL returned with -- output :  {output}")
-        LOG.info(f"YTB-DL returned with -- error :  {error}")
+        process = subprocess.run(["yt-dlp", "-x", "-f", "mp4", f'"ytsearch1:{phrase}"',"--geo-bypass", "--metadata-from-title", '"(?P<artist>.+?) - (?P<title>.+)"',"--xattrs","--embed-thumbnail--embed-thumbnail","-o","/home/pi/.usb-music/temp/request"], stdout=subprocess.PIPE)
+        # output, error = process.communicate()
+        # LOG.info(f"YTB-DL returned with -- output :  {output}")
+        # LOG.info(f"YTB-DL returned with -- error :  {error}")
 
         if error is None :
             return [{
